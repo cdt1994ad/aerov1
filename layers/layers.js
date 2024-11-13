223f3184ad39cb2de1505ccf2cd7f7cfc2fc4acf
cdt1994ad <afdurang@ut.edu.co>
2024-11-13 13:54:44 -0500

    v1
    v1

---
+++ layers/layers.js
@@ -0,0 +1,40 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_ne_10m_airports_1 = new ol.format.GeoJSON();
+var features_ne_10m_airports_1 = format_ne_10m_airports_1.readFeatures(json_ne_10m_airports_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ne_10m_airports_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ne_10m_airports_1.addFeatures(features_ne_10m_airports_1);
+var lyr_ne_10m_airports_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ne_10m_airports_1, 
+                style: style_ne_10m_airports_1,
+                popuplayertitle: "ne_10m_airports",
+                interactive: true,
+    title: 'ne_10m_airports<br />\
+    <img src="styles/legend/ne_10m_airports_1_0.png" /> major<br />\
+    <img src="styles/legend/ne_10m_airports_1_1.png" /> mid<br />\
+    <img src="styles/legend/ne_10m_airports_1_2.png" /> small<br />'
+        });
+
+lyr_OSMStandard_0.setVisible(true);lyr_ne_10m_airports_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_ne_10m_airports_1];
+lyr_ne_10m_airports_1.set('fieldAliases', {'scalerank': 'scalerank', 'featurecla': 'featurecla', 'type': 'type', 'name': 'name', 'abbrev': 'abbrev', 'location': 'location', 'gps_code': 'gps_code', 'iata_code': 'iata_code', 'wikipedia': 'wikipedia', 'natlscale': 'natlscale', });
+lyr_ne_10m_airports_1.set('fieldImages', {'scalerank': 'Range', 'featurecla': 'TextEdit', 'type': 'TextEdit', 'name': 'TextEdit', 'abbrev': 'TextEdit', 'location': 'TextEdit', 'gps_code': 'TextEdit', 'iata_code': 'TextEdit', 'wikipedia': 'TextEdit', 'natlscale': 'TextEdit', });
+lyr_ne_10m_airports_1.set('fieldLabels', {'scalerank': 'inline label - always visible', 'featurecla': 'inline label - always visible', 'type': 'inline label - always visible', 'name': 'inline label - always visible', 'abbrev': 'inline label - always visible', 'location': 'inline label - always visible', 'gps_code': 'inline label - always visible', 'iata_code': 'inline label - always visible', 'wikipedia': 'inline label - always visible', 'natlscale': 'inline label - always visible', });
+lyr_ne_10m_airports_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
